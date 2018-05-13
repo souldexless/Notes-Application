0fa7361e1efdbd3778c220f814308552c3b2940e
souldexless <22864235+souldexless@users.noreply.github.com>
2018-05-13 14:49:40 -0400

    First commit

---
+++ playground/arrow-function.js
@@ -0,0 +1,12 @@
+var square = x => x * x;
+console.log(square(9));
+
+var user = {
+	name: 'Andrew',
+	sayHiAlt () {
+		console.log(arguments);
+		console.log(`Hi. I'm ${this.name}`);
+	}
+};
+
+user.sayHiAlt(1, 2, 3);
---
+++ playground/json.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+var originalNote = {
+	title: 'Some title',
+	body: 'Some body'
+};
+
+//originalNoteString. set to json
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
